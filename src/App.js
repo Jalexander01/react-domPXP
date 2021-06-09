89a71e24d293ea3dff58da7e11ff3864473d178e
James Alexander <60295314+Jalexander01@users.noreply.github.com>
2021-06-09 09:11:42 -0500

    creeating new file app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./components/heading/Heading.component.jsx";
+import List from "./components/list/List.component.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./components/heading/Heading.component.jsx";
-import List from "./components/list/List.component.jsx";
+import App from "./App.js";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
